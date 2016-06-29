4c90a6b3dfd2d5f15a7982ff6cdb65b7eb2e474b
Danil Akhmetov <dakhmetov@mirantis.com>
2016-06-29 12:14:01 +0300

    little redirector caching, custom error page

--- redirect.py
+++ redirect.py
@@ -12,15 +12,20 @@ sf_tkn = parser.get('SalesForce', 'token')
 
 sf = Salesforce(custom_url=sf_url, username=sf_usr, password=sf_pwd, security_token=sf_tkn)
 
+kvs = {}
+
 app = Flask(__name__)
 
 @app.route('/case/<caseid>')
-def hello_world(caseid):
-    for case in sf.query("SELECT Id from Case where CaseNumber = '%d'" % int(caseid))['records']:
-        url = sf_url + '/console#%2f' + case['Id'] 
+def application(caseid):
+    if caseid in kvs:
+        url = sf_url + '/console#%2f' + kvs[caseid]
+    else:
+        for case in sf.query("SELECT Id from Case where CaseNumber = '%d'" % int(caseid))['records']:
+          kvs[caseid] = case['Id']
+          url = sf_url + '/console#%2f' + case['Id']
     return redirect(url, code=301)
 
 @app.errorhandler(500)
 def err(e):
-    return '<center><img src="http://pinkie.mylittlefacewhen.com/media/f/rsz/mlfw393_medium.png"></center>', 500
-
+    return '<head><style> html, body {margin:0;padding:0;} #footer {position:fixed;right:0;bottom:0;margin:0;width 75%} #footer img {width:100%;} </style></head> <center><h1>500 internal error</h1></center> <div id="footer"> <img src="http://pinkie.mylittlefacewhen.com/media/f/rsz/mlfw393_medium.png"></div>', 500
